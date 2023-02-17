dd0400bd0628d8a775375e5be1b0d09ad316aa3a
okuymakov <oleg.kuymakov@bk.ru>
2023-02-17 21:09:23 +0300

    Update Service Model

--- app/src/main/java/com/example/trajectoryolympiad/data/models/VKService.kt
+++ app/src/main/java/com/example/trajectoryolympiad/data/models/VKService.kt
@@ -1,8 +1,18 @@
 package com.example.trajectoryolympiad.data.models
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+@Serializable
 data class VKService (
     val name: String,
     val description: String,
+    @SerialName("icon_url")
     val iconUrl: String,
+    @SerialName("service_url")
     val serviceUrl: String,
+)
+
+@Serializable
+data class VKServices(
+    val items: List<VKService>
 )
