d046c577eba0d777f7f76c5c7e9881819d3359b8
okuymakov <oleg.kuymakov@bk.ru>
2023-02-17 16:00:53 +0300

    Add VK Service Model

---
+++ app/src/main/java/com/example/trajectoryolympiad/data/models/VKService.kt
@@ -0,0 +1,8 @@
+package com.example.trajectoryolympiad.data.models
+
+data class VKService (
+    val name: String,
+    val description: String,
+    val iconUrl: String,
+    val serviceUrl: String,
+)
