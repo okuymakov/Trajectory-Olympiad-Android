3737f3ed6765cdf9875b7ac51b93c2bee7dba3bf
okuymakov <oleg.kuymakov@bk.ru>
2023-02-18 09:46:54 +0300

    Add OnClick Handler

--- app/src/main/java/com/example/trajectoryolympiad/ui/services/adapter/ServicesAdapter.kt
+++ app/src/main/java/com/example/trajectoryolympiad/ui/services/adapter/ServicesAdapter.kt
@@ -10,7 +10,8 @@ import com.bumptech.glide.Glide
 import com.example.trajectoryolympiad.data.models.VKService
 import com.example.trajectoryolympiad.databinding.ServiceItemBinding
 
-class ServicesAdapter : RecyclerView.Adapter<ServicesAdapter.ServiceViewHolder>() {
+class ServicesAdapter(private val onClick: (VKService) -> Unit) :
+    RecyclerView.Adapter<ServicesAdapter.ServiceViewHolder>() {
 
     private val data = mutableListOf<VKService>()
 
@@ -20,6 +21,21 @@ class ServicesAdapter : RecyclerView.Adapter<ServicesAdapter.ServiceViewHolder>(
         data += newData
         notifyDataSetChanged()
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServiceViewHolder {
+        val binding = ServiceItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ServiceViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ServiceViewHolder, position: Int) {
+        val item = data[position]
+        holder.bind(item)
+        holder.itemView.setOnClickListener {
+            onClick(item)
+        }
+    }
+
+    override fun getItemCount() = data.size
 
     class ServiceViewHolder(binding: ServiceItemBinding) : RecyclerView.ViewHolder(binding.root) {
         private val serviceName: TextView
@@ -36,15 +52,4 @@ class ServicesAdapter : RecyclerView.Adapter<ServicesAdapter.ServiceViewHolder>(
 
         }
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServiceViewHolder {
-        val binding = ServiceItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ServiceViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: ServiceViewHolder, position: Int) {
-        holder.bind(data[position])
-    }
-
-    override fun getItemCount() = data.size
 }
