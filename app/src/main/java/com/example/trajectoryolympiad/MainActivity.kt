4e0d24742c4c07698b202cbfd6ba25a1494f36b2
okuymakov <oleg.kuymakov@bk.ru>
2023-02-18 09:49:07 +0300

    Update Main Activity

--- app/src/main/java/com/example/trajectoryolympiad/MainActivity.kt
+++ app/src/main/java/com/example/trajectoryolympiad/MainActivity.kt
@@ -2,12 +2,53 @@ package com.example.trajectoryolympiad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.addCallback
+import androidx.core.content.ContextCompat
+import com.example.trajectoryolympiad.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupOnBackPressedDispatcher()
+        setupToolbar()
+
     }
+
+    private fun setupOnBackPressedDispatcher() {
+        onBackPressedDispatcher.addCallback(this) {
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                supportFragmentManager.popBackStack()
+            } else {
+                finish()
+            }
+        }
+    }
+
+    private fun setupToolbar() {
+        val toolbar = binding.toolbar
+        supportFragmentManager.apply {
+            addOnBackStackChangedListener {
+                if (backStackEntryCount > 0) {
+                    toolbar.navigationIcon =
+                        ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_arrow_back_24)
+                    toolbar.setNavigationOnClickListener {
+                        popBackStack()
+                    }
+                } else {
+                    toolbar.navigationIcon = null
+                }
+            }
+        }
+    }
+
+    var toolbarTitle: String
+        get() = binding.toolbar.title.toString()
+        set(value) {
+            binding.toolbar.title = value
+        }
 }
